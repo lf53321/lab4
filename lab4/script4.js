7f05134d166503a9590a944a09efbc6e48e0cc5d
lf53321 <luka.friscic@fer.hr>
2023-12-07 16:21:37 +0100

    Initial commit

---
+++ lab4/script4.js
@@ -0,0 +1,153 @@
+class Rectangle {
+    constructor(height, width) {
+        this.height = height;
+        this.width = width;
+    }
+}
+
+
+const Rectangle = class {
+    constructor(height, width) {
+        this.height = height;
+        this.width = width;
+    }
+};
+
+const Rectangle = class Rectangle2 {
+    constructor(height, width) {
+        this.height = height;
+        this.width = width;
+    }
+};
+
+class Rectangle {
+    constructor(height, width) {
+        this.height = height;
+        this.width = width;
+    }
+}
+
+
+const Rectangle = class {
+    constructor(height, width) {
+        this.height = height;
+        this.width = width;
+    }
+};
+
+const Rectangle = class Rectangle2 {
+    constructor(height, width) {
+        this.height = height;
+        this.width = width;
+    }
+};
+
+class Rectangle {
+    constructor(height, width) {
+        this.height = height;
+        this.width = width;
+    }
+}
+
+
+const Rectangle = class {
+    constructor(height, width) {
+        this.height = height;
+        this.width = width;
+    }
+};
+
+const Rectangle = class Rectangle2 {
+    constructor(height, width) {
+        this.height = height;
+        this.width = width;
+    }
+};
+
+class Rectangle {
+    constructor(height, width) {
+        this.height = height;
+        this.width = width;
+    }
+}
+
+
+const Rectangle = class {
+    constructor(height, width) {
+        this.height = height;
+        this.width = width;
+    }
+};
+
+const Rectangle = class Rectangle2 {
+    constructor(height, width) {
+        this.height = height;
+        this.width = width;
+    }
+};
+
+class Rectangle {
+    constructor(height, width) {
+        this.height = height;
+        this.width = width;
+    }
+}
+
+
+const Rectangle = class {
+    constructor(height, width) {
+        this.height = height;
+        this.width = width;
+    }
+};
+
+const Rectangle = class Rectangle2 {
+    constructor(height, width) {
+        this.height = height;
+        this.width = width;
+    }
+};
+
+class Rectangle {
+    constructor(height, width) {
+        this.height = height;
+        this.width = width;
+    }
+}
+
+
+const Rectangle = class {
+    constructor(height, width) {
+        this.height = height;
+        this.width = width;
+    }
+};
+
+const Rectangle = class Rectangle2 {
+    constructor(height, width) {
+        this.height = height;
+        this.width = width;
+    }
+};
+
+class Rectangle {
+    constructor(height, width) {
+        this.height = height;
+        this.width = width;
+    }
+}
+
+
+const Rectangle = class {
+    constructor(height, width) {
+        this.height = height;
+        this.width = width;
+    }
+};
+
+const Rectangle = class Rectangle2 {
+    constructor(height, width) {
+        this.height = height;
+        this.width = width;
+    }
+};
